85d1495dc90292a48d0ee5cb9fdee9cebbcb0f8b
rain <rain@machine>
2012-04-02 17:54:17 +0300

    Application icon added.

--- SimpleWebKitBrowser/src/browser/SimpleBrowser.java
+++ SimpleWebKitBrowser/src/browser/SimpleBrowser.java
@@ -1,10 +1,11 @@
 package browser;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
@@ -19,11 +20,12 @@ public class SimpleBrowser {
 	 * @param args
 	 */
 	static void loadAddress(Text addressLine, Browser browser) {
+		final String httpPrefix = "http://";
 		String content = addressLine.getText();
 		if (content.equals("")) {
-			addressLine.setText("http://www.google.com");
-		} else if (!content.contains("http://")) {
-			addressLine.setText("http://" + content);
+			addressLine.setText(httpPrefix + SimpleBrowserConstants.DEFAULT_URL);
+		} else if (!content.contains(httpPrefix)) {
+			addressLine.setText(httpPrefix + content);
 		}
 		browser.setUrl(addressLine.getText());
 	}
@@ -32,11 +34,13 @@ public class SimpleBrowser {
 		//Create display and main shell.
 		Display display = new Display();
 		final Shell mainWindow = new Shell(display);
-		mainWindow.setMinimumSize(SimpleBrowserConstants.MIN_WINDOW_WIDTH, SimpleBrowserConstants.MIN_WINDOW_HEIGHT);
+		mainWindow.setMinimumSize(SimpleBrowserConstants.MIN_WINDOW_WIDTH,
+				SimpleBrowserConstants.MIN_WINDOW_HEIGHT);
 		mainWindow.setLocation(display.getClientArea().x +
 				(display.getClientArea().width - mainWindow.getSize().x) / 2, 
 				display.getClientArea().y + 
 				(display.getClientArea().height - mainWindow.getSize().y) / 2);
+		mainWindow.setImage(new Image(display, "AppIcon.png"));
 		
 		//Prepare main layout.
 		final RowLayout layout = new RowLayout(SWT.VERTICAL);
@@ -78,33 +82,23 @@ public class SimpleBrowser {
 		});
 
 		//Add main window's listener to control resizing of components.
-		mainWindow.addControlListener(new ControlListener() {
-			
+		mainWindow.addControlListener(new ControlAdapter() {
+
 			@Override
 			public void controlResized(ControlEvent e) {
-				System.out.println("Resized!");
-				System.out.println(mainWindow.getClientArea());
 				int widgetsWidth = mainWindow.getClientArea().width - 
 						(layout.marginLeft + layout.marginRight + layout.spacing * 2);
 				RowData rowData = new RowData(widgetsWidth, SWT.DEFAULT);
 				addressLine.setLayoutData(rowData);
 				startButton.setLayoutData(rowData);
-				System.out.println(addressLine.getSize().y + startButton.getSize().y);
 				browser.setLayoutData(new RowData(widgetsWidth, 
 						mainWindow.getClientArea().height -
 						(addressLine.computeSize(widgetsWidth, SWT.DEFAULT).y +
 								startButton.computeSize(widgetsWidth, SWT.DEFAULT).y + layout.marginTop * 3)));
 				browser.redraw();
-				System.out.println(browser.getLayoutData());
-				System.out.println(layout.spacing);
-			}
-			
-			@Override
-			public void controlMoved(ControlEvent e) {
-				// Empty.
 			}
 		});
-		
+
 		//Run.
 		mainWindow.pack();
 		mainWindow.open();
--- SimpleWebKitBrowser/src/browser/SimpleBrowserConstants.java
+++ SimpleWebKitBrowser/src/browser/SimpleBrowserConstants.java
@@ -3,6 +3,9 @@ package browser;
 public interface SimpleBrowserConstants {
 	public int MIN_WINDOW_WIDTH = 320;
 	public int MIN_WINDOW_HEIGHT = 240;
+	
 	public int DEFAULT_BROWSER_SIZE_X = 640;
 	public int DEFAULT_BROWSER_SIZE_Y = 480;
+	
+	public String DEFAULT_URL = "www.google.com"; 
 }
